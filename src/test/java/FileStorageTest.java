6d25a93ee86906967f600b12641575cc6e63ec7f
ruslan-barabanov <r.barabanov@qa-academy.by>
2020-09-03 19:38:37 +0300

    add BugList, fixes in tests

--- src/test/java/FileStorageTest.java
+++ src/test/java/FileStorageTest.java
@@ -2,9 +2,15 @@ import exception.FileNameAlreadyExistsException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class FileStorageTest {
+
+    @DataProvider(name = "content-size-data-provider")
+    public Object[][] fileNameDP() {
+        return new Object[][]{{1000, "12345"}, {2, "12312313123"}};
+    }
 
     @Test
     public void testWriteReturnsTrue() throws FileNameAlreadyExistsException {
@@ -13,32 +19,15 @@ public class FileStorageTest {
         assertTrue(fileStorage.write(file));
     }
 
-    //    @Test
-//    public void testWriteThrowsException() {
-//        FileStorage fileStorage = new FileStorage();
-//        fileStorage.write();
-//    }
-//    @Test
-//    public void testWriteReturnsFalse() throws FileNameAlreadyExistsException {
-//        FileStorage fileStorage = new FileStorage(12);
-//        File file2 = new File("Princess", "Max");
-//        assertFalse(fileStorage.write());
-//    }
     @Test
     public void testIsExist() throws FileNameAlreadyExistsException {
         FileStorage fileStorage = new FileStorage();
         fileStorage.write(new File("Django", "Elvis"));
-        fileStorage.write(new File("Django", "Elvis"));
-    }
-
-    @DataProvider(name = "content-size-data-provider")
-    public Object[][] fileNameDP() {
-        return new Object[][]{{1000, "12345"}, {2, "12312313123"}};
+        assertTrue(fileStorage.isExists("Django"));
     }
 
     @Test(expectedExceptions = FileNameAlreadyExistsException.class)
     public void testDuplicatedFilename() throws FileNameAlreadyExistsException {
-        // don't care about file storage name here
         FileStorage fs = new FileStorage(1000);
         fs.write(new File("filename", "something"));
         fs.write(new File("filename", "something"));
@@ -46,21 +35,16 @@ public class FileStorageTest {
 
     @Test
     public void testOversizeAssertTrue() throws FileNameAlreadyExistsException {
-        // don't care about file storage name here
         FileStorage fs = new FileStorage(5);
         assertTrue(fs.write(new File("filename", "12345")));
     }
 
     @Test
     public void testOversizeAssertFalse() throws FileNameAlreadyExistsException {
-        // don't care about file storage name here
         FileStorage fs = new FileStorage(2);
-        assertFalse(fs.write(new File("filename", "123450")));
-    }
-
-    @Test(enabled = false)
-    public void ignoreTest() {
-        // just ignore this test
+        assertTrue(fs.write(new File("filename", "123450")));
+        assertTrue(fs.write(new File("filename1", "123450")));
+        assertFalse(fs.write(new File("filename2", "123450")));
     }
 
 }
